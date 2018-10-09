40b5b4ebae75dd5a4a7c185a81b0b7864c3ab4d0
JOULKVA <johan@qualicom-conseil.fr>
2018-10-09 09:07:08 +0200

    V Design

--- app/src/main/java/com/example/dev_qualicom/gsca/accueil.java
+++ app/src/main/java/com/example/dev_qualicom/gsca/accueil.java
@@ -2,13 +2,9 @@ package com.example.dev_qualicom.gsca;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
 
 public class accueil extends AppCompatActivity {
-
-    TextView nom_contact;
-    TextView prenom_contact;
+    
     MembreSingleton membreSingleton;
 
     @Override
@@ -16,13 +12,7 @@ public class accueil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-
-        nom_contact = (TextView) findViewById(R.id.nom_contact);
-        prenom_contact = (TextView) findViewById(R.id.prenom_contact);
-
         membreSingleton = MembreSingleton.getInstance();
 
-        nom_contact.setText(membreSingleton.getMembre().getNom_contact());
-        prenom_contact.setText(membreSingleton.getMembre().getPrenom_contact());
     }
 }
