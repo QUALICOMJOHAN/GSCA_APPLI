0261a95caf4c777c73be277e19a32e738d2755d2
qualicomWendy <wendy@qualicom-conseil.fr>
2018-10-10 09:08:23 +0200

    wendy 1 10/10

---
+++ app/src/main/java/com/example/dev_qualicom/gsca/list_events_item.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.gsca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_events_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_events_item);
+    }
+}
