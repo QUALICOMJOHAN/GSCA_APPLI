21f73f96ba5a09f75af0d04280e3f7bf450b7e8a
JOULKVA <johan@qualicom-conseil.fr>
2018-10-10 11:33:34 +0200

    10/10 Jojo

---
+++ app/src/main/java/com/example/dev_qualicom/gsca/Member.java
@@ -0,0 +1,156 @@
+package com.example.dev_qualicom.gsca;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static android.Manifest.permission.CALL_PHONE;
+
+public class Member extends AppCompatActivity {
+
+    Membre membre;
+    ImageView logo_societe;
+    TextView societe_name;
+    TextView societe_desc;
+    TextView societe_address;
+    TextView societe_tel;
+    TextView societe_sector;
+    TextView societe_mainsector;
+    TextView societe_mail;
+    TextView societe_web;
+
+    ImageView member_profil;
+    TextView member_name;
+    TextView member_firstname;
+    TextView member_poste;
+    TextView member_number;
+    TextView member_mail;
+
+    ClubSingleton club;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+
+        OkHttpClient client = new OkHttpClient();
+        club = ClubSingleton.getInstance();
+
+        int id = getIntent().getIntExtra("id", 0);
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(club.getUrl()+"webService/getMembre.php").newBuilder();
+        urlBuilder.addQueryParameter("id", Integer.toString(id));
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                Gson gson = new Gson();
+                String json = response.body().string().toString();
+                membre = gson.fromJson(json, Membre.class);
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        logo_societe = (ImageView) findViewById(R.id.societe_logo);
+                        societe_name = (TextView) findViewById(R.id.societe_name);
+                        societe_desc = (TextView) findViewById(R.id.societe_desc);
+                        societe_address = (TextView) findViewById(R.id.societe_address);
+                        societe_tel = (TextView) findViewById(R.id.societe_tel);
+                        societe_sector = (TextView) findViewById(R.id.societe_sector);
+                        societe_mainsector = (TextView) findViewById(R.id.societe_mainsector);
+                        societe_mail = (TextView) findViewById(R.id.societe_mail);
+                        societe_web = (TextView) findViewById(R.id.societe_web);
+                        member_profil = (ImageView) findViewById(R.id.member_profil);
+                        member_name = (TextView) findViewById(R.id.member_name);
+                        member_firstname = (TextView) findViewById(R.id.membre_firstname);
+                        member_poste = (TextView) findViewById(R.id.membre_poste);
+                        member_number = (TextView) findViewById(R.id.membre_number);
+                        member_mail = (TextView) findViewById(R.id.membre_mail);
+
+                        Picasso.get().load(club.getUrl()+"../../img_membre"+membre.getImg_logo()).into(logo_societe);
+
+                        societe_name.setText(membre.getNom_societe());
+                        societe_desc.setText(membre.getDes_societe());
+                        societe_address.setText(membre.getAdresse_societe());
+                        societe_tel.setText(membre.getTel_societe());
+                        societe_sector.setText(membre.getSecteur_activite());
+                        societe_mainsector.setText(membre.getActivite_principal());
+                        societe_mail.setText(membre.getMail_societe());
+                        societe_web.setText(membre.getSite_societe());
+
+                        Picasso.get().load(club.getUrl()+"../../img_membre"+membre.getImg_profil()).into(member_profil);
+
+                        member_name.setText(membre.getNom_contact());
+                        member_firstname.setText(membre.getPrenom_contact());
+                        member_poste.setText(membre.getStatut_contact());
+                        member_number.setText(membre.getTel_contact());
+                        member_mail.setText(membre.getMail_contact());
+
+                        societe_tel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + societe_tel.getText()));
+
+                                if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                                    startActivity(intent);
+                                } else {
+                                    requestPermissions(new String[]{CALL_PHONE}, 1);
+                                }
+
+                            }
+                        });
+
+                        member_number.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + member_number.getText()));
+
+                                if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                                    startActivity(intent);
+                                } else {
+                                    requestPermissions(new String[]{CALL_PHONE}, 1);
+                                }
+
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/example/dev_qualicom/gsca/list_events.java
+++ app/src/main/java/com/example/dev_qualicom/gsca/list_events.java
@@ -1,13 +1,143 @@
 package com.example.dev_qualicom.gsca;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class list_events extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private List<Event_Class> listeEvent;
+
+    MembreSingleton membreSingleton = MembreSingleton.getInstance();
+
+    LinearLayoutManager llm = new LinearLayoutManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_events);
+        setContentView(R.layout.activity_list_members);
+
+        OkHttpClient client = new OkHttpClient();
+        ClubSingleton club = ClubSingleton.getInstance();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(club.getUrl()+"webService/getEvents.php").newBuilder();
+        urlBuilder.addQueryParameter("id", Integer.toString(membreSingleton.getMembre().getId()));
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                Gson gson = new Gson();
+                String json = response.body().string().toString();
+                listeEvent = gson.fromJson(json, new TypeToken<List<Event_Class>>(){}.getType());
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        recyclerView = (RecyclerView) findViewById(R.id.list_event_ecran);
+
+                        Log.e("TEST", listeEvent.get(0).getBackgroundColor());
+
+                        EventsAdapter eventsAdapter = new EventsAdapter();
+                        llm.setOrientation(LinearLayoutManager.VERTICAL);
+                        //recyclerView.setLayoutManager(llm);
+                        //recyclerView.setAdapter(eventsAdapter);
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    class EventsAdapter extends RecyclerView.Adapter<EventsHolder> {
+
+        @Override
+        public EventsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View vue = getLayoutInflater().inflate(R.layout.activity_list_events_item, null);
+            return new EventsHolder(vue);
+        }
+
+        @Override
+        public void onBindViewHolder(EventsHolder holder, int position) {
+
+            Event_Class event = listeEvent.get(position);
+            holder.bind(event);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return listeEvent.size();
+        }
+    }
+    class EventsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView nom_event;
+        private TextView date_event;
+        private TextView desc_event;
+
+        public EventsHolder(View itemView) {
+            super(itemView);
+
+            nom_event = itemView.findViewById(R.id.nom_event);
+            date_event = itemView.findViewById(R.id.date_event);
+            desc_event = itemView.findViewById(R.id.desc_event);
+
+        }
+
+        public void bind(Event_Class event){
+
+            nom_event.setText(event.getTitle());
+            date_event.setText(event.getStart());
+            desc_event.setText(event.getDesc_event());
+
+            this.itemView.setId(event.getId());
+            this.itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            /*int memberId = view.getId();
+
+            Intent intent = new Intent(Listmembers.this, Member.class);
+
+            intent.putExtra("id", memberId);
+
+            startActivity(intent);*/
+
+        }
     }
 }
